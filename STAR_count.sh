d838ec74b1063e3771af4dd82dbf5d335e117da8
Friederike (Freddie) Ehrhart <friederike.ehrhart@gmail.com>
2024-05-24 09:58:53 +0200

    Update STAR_count.sh

--- STAR_count.sh
+++ STAR_count.sh
@@ -1,31 +1,16 @@
-#!/bin/bash
-#SBATCH -A Research_Project-MRC164847 # research project to submit under.
-#SBATCH --export=ALL # export all environment variables to the batch job.
-#SBATCH -D . # set working directory to .
-#SBATCH -p mrcq # submit to the parallel test queue
-#SBATCH --time=24:00:00 # Maximum wall time for the job
-#SBATCH --nodes=1 # specify number of nodes.
-#SBATCH --ntasks-per-node=16 # specify number of processors.
-#SBATCH --mail-type=END # send email at job completion
-#SBATCH --mail-user=m.kouhsar@exeter.ac.uk # email address
-
-
-# Loading STAR software in the server
-module load STAR
-
-for i in ./*R1_001_fastp.fastq.gz
+for i in ~persistent/S/*R1_001.fastq.gz
 do
 
     R1=$i
     R2=${i/R1/R2}
     sample_name=$(echo $i| cut -d'/' -f 7)
-    sample_name=$(echo ${sample_name%R1_001_fastp.fastq.gz})
+    sample_name=$(echo ${sample_name%R1_001.fastq.gz})
     out_dir=./STAR_OutPut/${sample_name}
     temp_dir=./STAR_temp/${sample_name}
 
-    STAR --runThreadN 16 \
+    STAR --runThreadN 120 \
          --quantMode GeneCounts \
-         --genomeDir ./genome_index3_output/ \
+         --genomeDir ~persistent/STAR_OutPut/ \
          --outSAMtype None \
          --outFileNamePrefix ${out_dir}. \
          --readFilesIn $R1 $R2 \
