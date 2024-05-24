ca22583e9784e30aaef8f26d96f107cfb2f84dd2
Friederike (Freddie) Ehrhart <friederike.ehrhart@gmail.com>
2024-05-24 11:21:44 +0200

    Update STAR_GenomeIndexing.sh

--- STAR_GenomeIndexing.sh
+++ STAR_GenomeIndexing.sh
@@ -1,12 +1,8 @@
-export PATH=$PATH:root/persistent/software/STAR-2.7.11b/source
-# loading STAR software
-module load STAR
+export PATH=$PATH:~/persistent/software/STAR-2.7.11b/source
 
-# genome indexing
-
-STAR --runThreadN 50 \ 
+./STAR --runThreadN 50 \ 
      --runMode genomeGenerate \
-     --genomeDir ./STAR_OutPut/ \
-     --genomeFastaFiles ./S/ \
-     --sjdbGTFfile ./gencode.v45.annotation.gtf \
+     --genomeDir ~/persistent/STAR_OutPut/ \
+     --genomeFastaFiles ~/persistent/GRCh37.p13.genome.fa \
+     --sjdbGTFfile ~/persistent/gencode.v45.annotation.gtf \
      --sjdbOverhang 150\
