b6edc95757aae3b8f4b7f5a61baab8bceae0f8d2
Friederike (Freddie) Ehrhart <friederike.ehrhart@gmail.com>
2024-05-24 08:53:47 +0200

    Update STAR_GenomeIndexing.sh

--- STAR_GenomeIndexing.sh
+++ STAR_GenomeIndexing.sh
@@ -1,13 +1,3 @@
-#!/bin/bash
-#SBATCH -A Research_Project-MRC164847 # research project to submit under.
-#SBATCH --export=ALL # export all environment variables to the batch job.
-#SBATCH -p mrcq # submit to the parallel test queue
-#SBATCH --time=5:00:00 # Maximum wall time for the job
-#SBATCH --nodes=1 # specify number of nodes.
-#SBATCH --ntasks-per-node=16 # specify number of processors.
-#SBATCH --mail-type=END # send email at job completion
-#SBATCH --mail-user=m.kouhsar@exeter.ac.uk # email address
-
 export PATH=$PATH:root/persistent/software/STAR-2.7.11b/source
 # loading STAR software
 module load STAR
